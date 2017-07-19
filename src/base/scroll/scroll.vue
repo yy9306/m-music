0659a34892b6b278c27946eec60ae4b4eef4ed7a
yy9306 <1273460656@qq.com>
2017-07-19 17:28:50 +0800

    Update scroll.vue
    scroll

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -71,7 +71,7 @@
          })
        }
        if (this.beforeScroll) {
-         this.scroll.on('beforeScroll', () => {
+         this.scroll.on('beforeScrollStart', () => {
            this.$emit('beforeScroll')
          })
        }
